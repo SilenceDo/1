de84dffbdd9bed3f209b91096b25b032948da7fd
SilenceDo <dulin200820ok@163.com>
2016-08-24 23:45:28 +0800

    2

--- src/A/AA.java
+++ src/A/AA.java
@@ -1,8 +1,9 @@
 package A;
 
 public class AA {
-public static void main(String[] args) {
-	System.out.println("DDDDDDDDD");
-	System.out.println("DDDDDDDDD");
-}
+	public static void main(String[] args) {
+		System.out.println("DDDDDDDDD");
+
+		System.out.println("DDDDDDDDD");
+	}
 }
